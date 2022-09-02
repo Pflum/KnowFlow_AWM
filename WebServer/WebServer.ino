1720de4c4f148de574b79c899a836a6898e89217
Pflum <pflum@gmx.net>
2022-09-02 23:30:32 +0200

    .

---
+++ WebServer/WebServer.ino
@@ -0,0 +1,99 @@
+/*
+  Web Server
+
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ modified 02 Sept 2015
+ by Arturo Guadalupi
+ 
+ */
+
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <DS18B20.h>
+
+byte mac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+IPAddress ip(192, 168, 10, 203);
+DS18B20 ds(5);
+
+EthernetServer server(80);
+
+void setup() {
+  Ethernet.init(10);  // Most Arduino shields
+
+  Serial.begin(9600);
+  Serial.println("Ethernet WebServer Example");
+  Ethernet.begin(mac, ip);
+
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+  
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+void loop() {
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+
+        if (c == '\n' && currentLineIsBlank) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: application/json");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          
+          int tracker = 1;
+          char json[40];
+          
+          double temp = ds.getTempC();
+          char tempString[6];
+          dtostrf(temp, 5, 2, tempString);
+          //dtostrf(x, precision+3, precision, tempString);
+
+          Serial.println(temp);
+
+          
+          sprintf(json, "{\"trackerId\":%d,\"temp\": %s }", tracker, tempString);
+          client.println(json);
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
