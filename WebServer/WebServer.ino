2503b15cc7b3475b95dfe7d001fec50303797295
Pflum <pflum@gmx.net>
2022-09-03 23:20:24 +0200

    .

--- WebServer/WebServer.ino
+++ WebServer/WebServer.ino
@@ -19,20 +19,45 @@
 
 #include <SPI.h>
 #include <UIPEthernet.h>
+#include <EEPROM.h>
 #include <DS18B20.h>
+#include <DFRobot_PH.h>
+#include <DFRobot_EC10.h>
+#include <DFRobot_ORP_PRO.h>
 
+
+
+#define PH_PIN A2
+#define EC_PIN A1
+#define TEMP_PIN 5
+#define ORP_PIN A3
 byte mac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 IPAddress ip(192, 168, 10, 203);
-DS18B20 ds(5);
+long int orpOffset = -482;
 
+#define ORPVALUEADDR 0x00+5
+DS18B20 ds(TEMP_PIN);
 EthernetServer server(80);
+float orpVoltage,phVoltage,ecVoltage,phValue,ecValue,orpValue;
+char orpString[10],tempString[6],phString[10],ecString[10];
+double tempValue;
+int count = 0, orpCalibrationValue, orpenterCalibrationFlag = 0;
+int orpCalibrationFinish  = 0;
+DFRobot_PH ph;
+DFRobot_EC10 ec;
+DFRobot_ORP_PRO orp(0);
+
+
 
 void setup() {
   Ethernet.init(10);  // Most Arduino shields
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Ethernet WebServer Example");
   Ethernet.begin(mac, ip);
+  ph.begin();
+  ec.begin();
+  orp.setCalibration(orpOffset);
 
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
@@ -50,6 +75,26 @@ void setup() {
 }
 
 void loop() {
+  int tracker = 1;
+  char json[80];
+  // Temp
+  tempValue = ds.getTempC();
+  dtostrf(tempValue, 5, 2, tempString);
+  // pH
+  phVoltage = analogRead(PH_PIN)/1024.0*5000;
+  phValue = ph.readPH(phVoltage,tempValue);
+  dtostrf(phValue, 4, 2, phString);
+  // EC
+  ecVoltage = analogRead(EC_PIN)/1024.0*5000;
+  ecValue =  ec.readEC(ecVoltage,tempValue);
+  dtostrf(ecValue, 3, 1, ecString);
+  // ORP
+  orpVoltage = analogRead(ORP_PIN)/1024.0*5000;
+  orpValue =  orp.getORP(orpVoltage);
+  dtostrf(orpValue, 3, 1, orpString);
+  // DO
+
+  sprintf(json, "{\"trackerId\":%d,\"temp\": %s,\"ec\": %s,\"ph\": %s,\"orp\": %s }", tracker, tempString, ecString, phString, orpString);
   EthernetClient client = server.available();
   if (client) {
     Serial.println("new client");
@@ -66,18 +111,6 @@ void loop() {
           client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
           
-          int tracker = 1;
-          char json[40];
-          
-          double temp = ds.getTempC();
-          char tempString[6];
-          dtostrf(temp, 5, 2, tempString);
-          //dtostrf(x, precision+3, precision, tempString);
-
-          Serial.println(temp);
-
-          
-          sprintf(json, "{\"trackerId\":%d,\"temp\": %s }", tracker, tempString);
           client.println(json);
           break;
         }
@@ -96,4 +129,70 @@ void loop() {
     client.stop();
     Serial.println("client disconnected");
   }
+  if(count == 0) {
+    Serial.println(json);
+    count = 5;
+  }
+  count--;
+  char cmd[10];
+  if(readSerial(cmd)){
+    strupr(cmd);
+    Serial.println(cmd);
+    if(strstr(cmd,"PH")){
+      ph.calibration(phVoltage,tempValue,cmd);       //PH calibration process by Serail CMD
+    }
+    if(strstr(cmd,"EC")){
+      ec.calibration(ecVoltage,tempValue,cmd);       //EC calibration process by Serail CMD
+    }
+    if(strstr(cmd,"ORP")){
+      if(strstr(cmd,"ENTERORP")){
+        Serial.println();
+        Serial.println(F(">>>Enter ORP Calibration Mode<<<"));
+        Serial.println(F(">>>Please disconnect probe and press the botton<<<"));
+        Serial.println();
+        orpenterCalibrationFlag = 1;
+      }
+      else if(strstr(cmd,"EXITORP") && orpenterCalibrationFlag){
+        if(orpCalibrationFinish){
+          orp.setCalibration(orpCalibrationValue);
+          Serial.print(F(">>>Calibration Successful"));
+        } else {
+          Serial.print(F(">>>Calibration Failed"));
+        }
+        Serial.println(F(", Exit ORP Calibration Mode<<<"));
+        Serial.print(orpCalibrationValue);
+        Serial.println(F(" in die Variable orpOffset schreiben und neu flashen sonst ist diese eistellung nach einem Stromausfall weg!")); 
+        Serial.println();
+        orpenterCalibrationFlag = 0;
+        orpCalibrationFinish  = 0;
+      }
+      else if(strstr(cmd,"CALORP")){
+        orpCalibrationValue = orp.calibrate(analogRead(ORP_PIN)/1024.0*5000);
+        orpCalibrationFinish  = 1;
+        Serial.println();
+        Serial.println(F(">>>Send EXITORP to Save and Exit<<<")); 
+        Serial.println();
+      }
+    }
+  }
+}
+
+
+int i = 0;
+bool readSerial(char result[]){
+    while(Serial.available() > 0){
+        char inChar = Serial.read();
+        if(inChar == '\n'){
+             result[i] = '\0';
+             Serial.flush();
+             i=0;
+             return true;
+        }
+        if(inChar != '\r'){
+             result[i] = inChar;
+             i++;
+        }
+        delay(1);
+    }
+    return false;
 }
